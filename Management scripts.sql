9ca5c3e49066dd9e463fcf7719516c7d49941d44
Steini <steini@straumnes.is>
2014-01-08 10:42:52 +0000

    Management script added

---
+++ Management scripts.sql
@@ -0,0 +1,23 @@
+/*====================================================================================================
+
+  Management scripts for SQL Server
+
+  Author: Steini Jonsson
+
+====================================================================================================*/
+
+----------------------------------------------------------------------------------------------------
+-- List jobs from Reporting server (with usefule names)
+----------------------------------------------------------------------------------------------------
+select  sc.ScheduleID       [SQLAgent_Job_Name]
+      , j.[name]            [Job name]
+      , s.Description       [sub_desc]
+      , s.DeliveryExtension [sub_delExt]
+      , c.Name              [ReportName]
+      , c.Path              [ReportPath]
+from    ReportServer..ReportSchedule   rs
+join    ReportServer..Schedule      sc  on  rs.ScheduleID = sc.ScheduleID 
+join    ReportServer..Subscriptions s   on  rs.SubscriptionID = s.SubscriptionID 
+join    ReportServer..[Catalog]     c   on  rs.ReportID = c.ItemID 
+                                        and s.Report_OID = c.ItemID
+join    msdb..sysjobs               j   on  sc.ScheduleID = j.job_id
