8d359563f4c21510c8adf2807a7acec28d4ff7be
SteiniJonsson <steini@straumnes.is>
2014-04-02 22:04:35 +0000

    Update Management scripts.sql
    Create database templated added.

--- Management scripts.sql
+++ Management scripts.sql
@@ -23,5 +23,59 @@ join    ReportServer..[Catalog]     c   on  rs.ReportID = c.ItemID
 join    msdb..sysjobs               j   on  convert ( sysname, sc.ScheduleID ) = j.name
 
 ----------------------------------------------------------------------------------------------------
+-- Create database template with primary filegroup for system objects and a separated filegroup
+-- for data.
+----------------------------------------------------------------------------------------------------
+use [master]
+create database [<database_name, nvarchar(50), DB1>]
+containment = none
+on primary 
+( 
+  name = N'<database_name, nvarchar(50), DB1>_Primary', 
+  filename = N'C:\SQLData\Default\<database_name, nvarchar(50), DB1>_Primary.mdf' , 
+  size = 50 MB,
+  maxsize = 1 GB, 
+  filegrowth = 50 MB
+)
+log on 
+( 
+  name = N'<database_name, nvarchar(50), DB1>_Log', 
+  filename = N'C:\SQLTLogs\Default\<database_name, nvarchar(50), DB1>_Log.ldf' , 
+  size = 100 MB, 
+  maxsize = 5 GB, 
+  filegrowth = 100 MB 
+)
+go
+
+alter database [<database_name, nvarchar(50), DB1>] set compatibility_level = 120
+alter database [<database_name, nvarchar(50), DB1>] set recovery simple 
+go
+
+use [<database_name, nvarchar(50), DB1>]
+go
+
+alter database [<database_name, nvarchar(50), DB1>] add filegroup [Data1] 
+go
+
+alter database [<database_name, nvarchar(50), DB1>]
+add file
+( 
+  name = N'<database_name, nvarchar(50), DB1>_Data1', 
+  filename = N'C:\SQLData\Default\<database_name, nvarchar(50), DB1>_Data1.ndf' , 
+  size = 50 MB, 
+  maxsize = 5 GB, 
+  filegrowth = 50 MB
+)
+to filegroup [Data1]
+
+if not exists (select name from sys.filegroups where is_default=1 and name = N'Data1') 
+alter database [<database_name, nvarchar(50), DB1>] modify filegroup [Data1] default
+go
+
+exec sp_changedbowner @loginame = N'sa'
+go
+
+use [master]
+----------------------------------------------------------------------------------------------------
 -- 
 ----------------------------------------------------------------------------------------------------
