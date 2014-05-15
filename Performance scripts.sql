7c9adb3d28a7c5146b25313f71ff30231e9b7cff
SteiniJonsson <steini@straumnes.is>
2014-05-15 13:10:11 +0000

    Update Performance scripts.sql

--- Performance scripts.sql
+++ Performance scripts.sql
@@ -18,12 +18,24 @@ exec sp_updatestats
 ----------------------------------------------------------------------------------------------------
 -- List index fragmentations
 ----------------------------------------------------------------------------------------------------
-select object_name ( object_id )
-     , index_id,index_type_desc,index_level
-     , avg_fragmentation_in_percent
-     , avg_page_space_used_in_percent
-     , page_count
-from   sys.dm_db_index_physical_stats(db_id(), null, null, null , null );
+with cte as 
+(
+  select schema_name ( o.schema_id )      [Schema name]
+       , o.name                           [Object name]
+       , i.index_id                       [Index Id]
+       , x.name                           [Index name]
+       , i.index_type_desc                [Index type description]
+       , i.index_level                    [Index level]
+       , i.avg_fragmentation_in_percent   [Index framentation %]
+       , i.avg_page_space_used_in_percent [Average page space used %]
+       , i.page_count                     [Page count]
+  from sys.dm_db_index_physical_stats(db_id(), null, null, null , null ) i
+  join sys.objects o on  i.object_id = o.object_id
+  join sys.indexes x on  i.object_id = x.object_id
+                     and i.index_id = x.index_id
+)
+select * from cte
+where [Object name] = '<TableName>'
 
 ----------------------------------------------------------------------------------------------------
 -- 
