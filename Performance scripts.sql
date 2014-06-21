cfcae1fd3b887d314ac923ce19e4e8c3b1e26035
SteiniJonsson <steini@straumnes.is>
2014-06-21 20:45:43 +0000

    Einhverjar breytingar

--- Performance scripts.sql
+++ Performance scripts.sql
@@ -17,13 +17,21 @@ exec sp_updatestats
 
 ----------------------------------------------------------------------------------------------------
 -- List index fragmentations
+-- Rule of thumb: if avg_page_space_used_in_percent > 5% and <= 30% you should reorganize index
+--                if avg_page_space_used_in_percent > 30% you should rebuild index
 ----------------------------------------------------------------------------------------------------
-select object_name ( object_id )
-     , index_id,index_type_desc,index_level
-     , avg_fragmentation_in_percent
-     , avg_page_space_used_in_percent
-     , page_count
-from   sys.dm_db_index_physical_stats(db_id(), null, null, null , null );
+select   s.name + '.' + o.name                 [Table/view name]
+       , o.type_desc                           [Object type]
+       , index_id                              [Index ID]
+       , index_type_desc                       [Index type]
+       , index_level                           [Index level]
+       , avg_fragmentation_in_percent          [Fragmentation in %]
+       , fragment_count                        [Fragement count]
+       , page_count                            [Page count]
+from     sys.dm_db_index_physical_stats(db_id(), null, null, null , null ) i
+join     sys.all_objects                                                   o on i.object_id = o.object_id
+join     sys.schemas                                                       s on o.schema_id = s.schema_id
+order by avg_fragmentation_in_percent desc
 
 ----------------------------------------------------------------------------------------------------
 -- 
